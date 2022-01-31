4c30b927760a9f1ad1e5577d4efb3934771afdca
Gian <gianfranco25032@gmail.com>
2022-01-31 11:10:26 -0300

    Cambios para la 1ra entrega

--- js/page.js
+++ js/page.js
@@ -1,4 +1,5 @@
-let usuario = prompt("Ingresa tu nombre de usuario: ");
+function Entrada() {
+    let usuario = prompt("Ingresa tu nombre de usuario: ");
     if (usuario == "gian" || usuario == "Gian" || usuario == "GIAN") {
         alert("¡Bienvenido " + usuario + "!");
     } else {
@@ -6,29 +7,30 @@ let usuario = prompt("Ingresa tu nombre de usuario: ");
             alert("Usuario incorrecto, ingrese su nombre nuevamente.");
             usuario = prompt("Ingresa tu nombre de usuario: ");
         } while (usuario == "" || usuario == " " || usuario == null);
+    }
 }
+Entrada();
 
 const propiedades = ["Casa en venta", "Departamento en alquiler"];
 propiedades.push("***Terrenos en construcción...***".toUpperCase());
+
 alert("Las propiedades disponibles son: ");
 propiedades.forEach(propiedades => alert(propiedades));
-/* for (let i = 0; i < propiedades.length; i++){
-    alert(propiedades[i]);
-} */
 
-const propiedad1 = {
-    negocio: "venta",
-    precio: 234900,
-    ubicación: "Carapachay",
-    condición: "amueblada"
+class Propiedades{
+    constructor(negocio, precio, ubicacion) {
+        this.negocio = negocio;
+        this.precio = precio;
+        this.ubicacion = ubicacion;
 }
-alert("Casa en " + propiedad1.negocio + " ubicada en " + propiedad1.ubicación + ", su precio ronda los US$" + propiedad1.precio + " y está " + propiedad1.condición + " en todo su interior.");
+}
+const casa = new Propiedades("venta", 234900, "Carapachay");
+alert("Casa en " + casa.negocio + " ubicada en " + casa.ubicacion + ", su precio ronda los US$" + casa.precio);
 
-const propiedad2 = {
-    negocio: "alquiler",
-    precio: 40000,
-    expensas: 4000,
-    ubicación: "Lisandro de La Torre",
-    estado: "nuevo"
-}
-alert(`Departamento ${propiedad2.estado} en ${propiedad2.negocio}, ubicado a mts de la estación de ${propiedad2.ubicación}. Su precio ronda los ${propiedad2.precio}ARS + ${propiedad2.expensas} de expensas.`);
+const dpto = new Propiedades("alquiler", 40000, "Lisandro de La Torre");
+alert(`Departamento en ${dpto.negocio}, ubicado a mts de la estación de ${dpto.ubicacion}. Su precio ronda los ${dpto.precio}ARS.`);
+
+/* const propCasa = Propiedades.filter((el) => el.negocio.includes("venta")); también probe usando casa.filter/dpto.filter pero no funcionó
+const propDpto = Propiedades.filter((el) => el.negocio.includes("alquiler"));
+
+alert(propCasa); */
