980f53729ea91952daa6f61d6458e4c21a45eda4
Gian <gianfranco25032@gmail.com>
2022-02-02 23:54:34 -0300

    Agregue DOM

--- js/page.js
+++ js/page.js
@@ -1,4 +1,4 @@
-function Entrada() {
+/* function Entrada() {
     let usuario = prompt("Ingresa tu nombre de usuario: ");
     if (usuario == "gian" || usuario == "Gian" || usuario == "GIAN") {
         alert("¡Bienvenido " + usuario + "!");
@@ -28,9 +28,20 @@ const casa = new Propiedades("venta", 234900, "Carapachay");
 alert("Casa en " + casa.negocio + " ubicada en " + casa.ubicacion + ", su precio ronda los US$" + casa.precio);
 
 const dpto = new Propiedades("alquiler", 40000, "Lisandro de La Torre");
-alert(`Departamento en ${dpto.negocio}, ubicado a mts de la estación de ${dpto.ubicacion}. Su precio ronda los ${dpto.precio}ARS.`);
+alert(`Departamento en ${dpto.negocio}, ubicado a mts de la estación de ${dpto.ubicacion}. Su precio ronda los ${dpto.precio}ARS.`); */
 
 /* const propCasa = Propiedades.filter((el) => el.negocio.includes("venta")); también probe usando casa.filter/dpto.filter pero no funcionó
 const propDpto = Propiedades.filter((el) => el.negocio.includes("alquiler"));
 
 alert(propCasa); */
+
+const titulo = document.getElementById("titulo");
+titulo.innerText = "texto de prueba";
+
+const parrafo = document.getElementById("parrafo");
+parrafo.remove();
+
+const h6 = document.createElement("h6");
+h6.innerHTML = "<h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h6>";
+document.body.append(h6);
+
