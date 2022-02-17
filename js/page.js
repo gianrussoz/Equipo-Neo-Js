b2521cb0b328a004effcb970b74bf89e3d49c5c8
Gian <gianfranco25032@gmail.com>
2022-02-16 21:53:14 -0300

    Cambios para la 2da entrega

--- js/page.js
+++ js/page.js
@@ -1,44 +1,94 @@
-/* function Entrada() {
-    let usuario = prompt("Ingresa tu nombre de usuario: ");
-    if (usuario == "gian" || usuario == "Gian" || usuario == "GIAN") {
-        alert("¡Bienvenido " + usuario + "!");
-    } else {
-        do {
-            alert("Usuario incorrecto, ingrese su nombre nuevamente.");
-            usuario = prompt("Ingresa tu nombre de usuario: ");
-        } while (usuario == "" || usuario == " " || usuario == null);
-    }
+// Modal
+let container = document.getElementById("container");
+container.innerHTML =
+`<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" id="botonIS"><i class="fa-solid fa-house-user"></i></button>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Iniciar sesión</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-group">
+              <label for="exampleInputEmail1">Ingrese su nombre de usuario</label>
+            <input type="email" class="form-control" id="inputLogIn" aria-describedby="emailHelp" placeholder="Ej: juancruz22" required>
+            <small id="emailHelp" class="form-text text-muted">No compartimos sus datos con nadie.</small>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" id="botonEnviar" data-dismiss="modal" aria-label="Close">Entrar</button>
+      </div>
+    </div>
+  </div>
+</div>`
+
+// Log in
+let boton = document.getElementById("botonEnviar");
+
+boton.addEventListener("click", logIn);
+function logIn() {
+  let usuario = document.getElementById("inputLogIn").value;
+  if (usuario === "gian" || usuario === "Gian" || usuario === "GIAN") {
+    Toastify({
+        text: `👤 Bienvenido ${usuario}!`,
+        duration: 2000,
+        style: {
+        background: 'linear-gradient(to top, #48c6ef 0%, #6f86d6 100%)', color: '#f6f6f6'
+      }
+    }).showToast();
+    localStorage.setItem("nombredeUsuario", JSON.stringify(usuario));
+/*  $('#exampleModal').hide();
+    $('.fade').hide(); */
+    ;} else {
+    do {
+      Toastify({
+        text: `❌ Usuario incorrecto, ingrese su nombre nuevamente.`,
+        duration: 2000,
+        style: {
+        background: 'linear-gradient(to top, #cb2d3e 0%, #ef473a 100%)', color: '#f6f6f6'
+      }
+    }).showToast();
+      usuario = console.log("Ingresa tu nombre de usuario: ");
+    } while (usuario === "" || usuario === " " || usuario === null);
+  }
 }
-Entrada();
+function recuperarNU() {
+  let usuarioRecuperado = JSON.parse(localStorage.getItem('nombredeUsuario'));
+  console.log(usuarioRecuperado);
+}
+recuperarNU();
 
-const propiedades = ["Casa en venta", "Departamento en alquiler"];
-propiedades.push("***Terrenos en construcción...***".toUpperCase());
+// Filtrado
+const botonForm = document.getElementById("botonForm").value;
 
-alert("Las propiedades disponibles son: ");
-propiedades.forEach(propiedades => alert(propiedades));
-
+const propiedades = [];
 class Propiedades{
     constructor(negocio, precio, ubicacion) {
         this.negocio = negocio;
         this.precio = precio;
         this.ubicacion = ubicacion;
-}
-}
+}}
 const casa = new Propiedades("venta", 234900, "Carapachay");
-alert("Casa en " + casa.negocio + " ubicada en " + casa.ubicacion + ", su precio ronda los US$" + casa.precio);
-
 const dpto = new Propiedades("alquiler", 40000, "Lisandro de La Torre");
-alert(`Departamento en ${dpto.negocio}, ubicado a mts de la estación de ${dpto.ubicacion}. Su precio ronda los ${dpto.precio}ARS.`); */
+/* console.log (`Departamento en ${dpto.negocio}, ubicado a mts de la estación de ${dpto.ubicacion}. Su precio ronda los ${dpto.precio}ARS.`)
+console.log(`Casa en ${casa.negocio}, ubicada en ${casa.ubicacion}, su precio ronda los US$${casa.precio}`) */
 
-/* const propCasa = Propiedades.filter((el) => el.negocio.includes("venta")); también probe usando casa.filter/dpto.filter pero no funcionó
-const propDpto = Propiedades.filter((el) => el.negocio.includes("alquiler"));
-alert(propCasa);*/
+propiedades.push(casa, dpto);
+propiedades.forEach(propiedades => console.log(propiedades));
 
-const boton = document.getElementById("botonForm");
-boton.addEventListener("click", formEnviado);
-function formEnviado() {
-    console.log("Formulario enviado")
-};
+const filtroCasa = propiedades.find((el) => el.negocio === "venta");
+const filtroDpto = propiedades.find((el) => el.negocio === "alquiler");
+console.log(JSON.stringify(filtroCasa));
+console.log(JSON.stringify(filtroDpto));
+/* const botonForm = document.getElementById("botonForm");
 
-const input = document.getElementsByClassName("texto");
-input.onkeydown = () => { console.log("Tecla presionada") };
+botonForm.addEventListener("click", ()=>{
+  carritoDeCompras = carritoDeCompras.filter(elemento => elemento.id != productoAgregar.id)));
+}*/
+//? Quise filtrar mediante el formulario del index, pero no llego a terminarlo
+
+
